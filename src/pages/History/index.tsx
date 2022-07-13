2d8af28b3758b5e9749b4c9481b03a125868d747
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-07-13 12:33:36 -0300

    layout history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,47 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
-  return <div>History</div>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td> <Status statusColor="green">Concluído</Status></td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td> <Status statusColor="red">Concluído</Status></td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td> <Status statusColor='red'>Concluído</Status></td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td> <Status statusColor="yellow">Concluído</Status></td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
