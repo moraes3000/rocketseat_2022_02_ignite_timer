116ad54f8265f2411b3e931922e7e3d05e343ccd
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-07-13 12:09:10 -0300

    layout home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,45 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TastInput } from "./styles";
+
 export function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <HomeContainer>
+        <form>
+          <FormContainer>
+            <label htmlFor="task">Vou trabalhar em</label>
+            <TastInput type="text" list='task-sugestion' id="task" placeholder="Dê um nome para o seu projeto" />
+
+            <datalist id='task-sugestion'>
+              <option value='projeto 1' />
+              <option value='projeto 2' />
+            </datalist>
+
+            <label htmlFor="minutesAmount">durante</label>
+            <MinutesAmountInput
+              type="number"
+              id="minutesAmount"
+              placeholder="00"
+              step={5}
+              min={0}
+              max={60}
+            />
+            <span>Minutos</span>
+          </FormContainer>
+
+          <CountdownContainer>
+            <span>0</span>
+            <span>0</span>
+            <Separator>:</Separator>
+            <span>0</span>
+            <span>0</span>
+          </CountdownContainer>
+
+          <StartCountdownButton type="submit" id="">
+            <Play size={24} /> Começar
+          </StartCountdownButton>
+        </form>
+      </HomeContainer>
+    </>
+  )
 }
