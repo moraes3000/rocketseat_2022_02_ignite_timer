180ccf07b33dc49a3e4d578a9d39d6921d5fb432
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-07-13 10:47:37 -0300

    Layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Router } from './Router'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
 import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
