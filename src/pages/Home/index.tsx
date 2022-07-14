38211ff1b9a2b6ce320e702dfe913d5202086b98
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-07-14 11:41:39 -0300

    Criando countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -31,6 +31,7 @@ interface Cycle {
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
 
   const { register, handleSubmit, watch, formState, reset } = useForm<NewCreateNewCycle>({
@@ -62,7 +63,16 @@ export function Home() {
   // console.log(formState.errors)
 
   const activeCycle = cycles.find(cycles => cycles.id == activeCycleId)
-  console.log(activeCycle)
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, "0")
+  const seconds = String(secondsAmount).padStart(2, "0")
+
   return (
     <>
       <HomeContainer>
@@ -96,11 +106,11 @@ export function Home() {
           </FormContainer>
 
           <CountdownContainer>
-            <span>0</span>
-            <span>0</span>
+            <span>{minutes[0]}</span>
+            <span>{minutes[1]}</span>
             <Separator>:</Separator>
-            <span>0</span>
-            <span>0</span>
+            <span>{seconds[0]}</span>
+            <span>{seconds[1]}</span>
           </CountdownContainer>
 
           <StartCountdownButton type="submit" id="" disabled={isSubmitDisabled}>
